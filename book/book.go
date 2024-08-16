9debcf5d604d4f729f104a3911d0d36ffcdfbd67
H4XOR_ <hakroot07@gmail.com>
2024-08-17 00:05:22 +0200

    Add CRUD in book

--- book/book.go
+++ book/book.go
@@ -1,1 +1,57 @@
 package book
+
+import (
+	"fmt"
+	"time"
+)
+
+type PhoneBook map[string]PhoneNumber
+
+type PhoneNumber struct {
+	Number       string
+	LastUpdateAt int64
+}
+
+func (book *PhoneBook) Add(name, phoneNum string) error {
+	if _, exists := (*book)[name]; exists {
+		return fmt.Errorf("name %s already exists", name)
+	}
+
+	// n := new(PhoneNumber)
+	// n.Number = phoneNum
+	// n.LastUpdateAt = time.Now().Unix()
+	// (*book)[name] = *n
+	(*book)[name] = PhoneNumber{
+		Number:       phoneNum,
+		LastUpdateAt: time.Now().Unix(),
+	}
+	return nil
+}
+
+func (book *PhoneBook) Get(name string) (PhoneNumber, error) {
+	if numberData, exists := (*book)[name]; exists {
+		return numberData, nil
+	}
+
+	return PhoneNumber{}, fmt.Errorf("no entry found for %s", name)
+}
+
+func (book *PhoneBook) Update(name, newPhoneNum string) error {
+	if _, exists := (*book)[name]; !exists {
+		return fmt.Errorf("name %s does not exist", name)
+	}
+
+	(*book)[name] = PhoneNumber{
+		Number:       newPhoneNum,
+		LastUpdateAt: time.Now().Unix(),
+	}
+	return nil
+}
+
+func (book *PhoneBook) Delete(name string) error {
+	if _, exists := (*book)[name]; !exists {
+		return fmt.Errorf("name %s does not exist", name)
+	}
+	delete(*book, name)
+	return nil
+}
