b6cedce64849cb9987bc852300dadabf93f70c06
H4XOR_ <hakroot07@gmail.com>
2024-08-16 20:38:52 +0200

    Case update

--- main.go
+++ main.go
@@ -8,10 +8,10 @@ import (
 )
 
 func main() {
-	phonebook := make(map[string]string)
+	phoneBook := make(map[string]string)
 
 	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Println("Phonebook")
+	fmt.Println("PhoneBook")
 	fmt.Println("Available commands: add,get,update,exit")
 
 	for {
@@ -32,12 +32,12 @@ func main() {
 				continue
 			}
 			name, number := kv[0], kv[1]
-			phonebook[name] = number
+			phoneBook[name] = number
 			fmt.Printf("Added/Updated: %s -> %s\n", name, number)
 		case "get":
 			// get alice
 			name := parts[1]
-			number, exists := phonebook[name]
+			number, exists := phoneBook[name]
 			if exists {
 				fmt.Printf("Number for %s is %s\n", name, number)
 			} else {
@@ -45,14 +45,28 @@ func main() {
 			}
 		case "delete":
 			name := parts[1]
-			_, exists := phonebook[name]
+			_, exists := phoneBook[name]
 			if exists {
-				delete(phonebook, name)
+				delete(phoneBook, name)
 				fmt.Printf("Deleted entry for %s\n", name)
 			} else {
 				fmt.Printf("No entry to delete for %s\n", name)
 			}
-
+		case "update":
+			//update alise=123456456
+			kv := strings.SplitN(parts[1], "=", 2)
+			if len(kv) != 2 {
+				fmt.Println("Invalid format. Use: update name=number")
+				continue
+			}
+			name, newNumber := kv[0], kv[1]
+			_, exists := phoneBook[name]
+			if exists {
+				phoneBook[name] = newNumber
+				fmt.Printf("Updated %s -> %s\n", name, newNumber)
+			} else {
+				fmt.Println("No such entry to update.")
+			}
 		}
 	}
 }
