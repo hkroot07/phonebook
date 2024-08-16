dc82bf86e705eacb25b2ea3b6b9bfffd9c84bba4
H4XOR_ <hakroot07@gmail.com>
2024-08-16 20:14:10 +0200

    Add switch command

--- main.go
+++ main.go
@@ -23,5 +23,17 @@ func main() {
 		// exemple : add ~ alice=123
 		parts := strings.SplitN(line, " ", 2)
 		command := parts[0]
+
+		switch command {
+		case "add":
+			kv := strings.SplitN(parts[1], "=", 2)
+			if len(kv) != 2 {
+				fmt.Println("Invalid format. Use: add name=number")
+				continue
+			}
+			name, number := kv[0], kv[1]
+			phonebook[name] = number
+			fmt.Printf("Added/Updated: %s -> %s\n", name, number)
+		}
 	}
 }
